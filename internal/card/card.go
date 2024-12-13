095f5600ab35c6242d0b34391f80a82e867a87cf
Ke126 <ke126@outlook.com>
2024-12-12 21:56:08 -0500

    first card implementation

--- internal/card/card.go
+++ internal/card/card.go
@@ -3,5 +3,8 @@ package card
 import "html/template"
 
 func NewTemplate() (*template.Template, error) {
-	return template.New("card.svg").ParseFiles("templates/card.svg")
+	return template.New("card.svg").Funcs(template.FuncMap{
+		"add": func(a, b int) int { return a + b },
+		"mul": func(a, b int) int { return a * b },
+	}).ParseFiles("templates/card.svg")
 }
